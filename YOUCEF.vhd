b222f169d33f56517dcfa9163395679c338ee942
Mieartt <ard.iheb@gmail.com>
2025-02-01 22:56:32 +0100

    first commit

---
+++ YOUCEF.vhd
@@ -0,0 +1,12 @@
+library ieee;
+use.ieee_std_logic_1164.all;
+entity youcef is
+    port (
+        wednin : in std_logic;
+        fom : out std_logic
+    );
+architecture chbab of youcef is
+signal nice_hair : std_logic;
+signal head : std_logic;
+begin
+end chbab;
